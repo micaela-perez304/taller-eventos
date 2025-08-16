d1a9645aaeea468a0a59d6932f27a66fab4da797
micaela-perez <micaelaperez501@gmail.com>
2025-08-16 00:01:54 -0300

    Update eventos.js

--- eventos.js
+++ eventos.js
@@ -1,3 +1,10 @@
+const boton = document.querySelector("button");
+
+boton.addEventListener("click", function(event) {
+    event.stopPropagation(); // Evita que el click llegue al div
+    alert("Hola! Soy el botón");
+});
+
 document.getElementById("contenedor").addEventListener("click", function() {
     alert("Hola! Soy el div");
 });
